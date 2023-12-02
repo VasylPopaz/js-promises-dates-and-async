145b2a62a6c8583deef14f3ac40945b92a8fa0f6
VasylPopaz <145382665+VasylPopaz@users.noreply.github.com>
2023-12-02 19:19:54 +0000

    Deploying to gh-pages from @ VasylPopaz/goit-js-hw-09@5fcb54a5c02b4fbf23e4fb821c9f563912d764a1 🚀

---
+++ 01-color-switcher.34b32e3b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;let d=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.34b32e3b.js.map
