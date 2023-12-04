ec44b5587c3c454a03445f12e6dee5c859275362
VasylPopaz <pafos1997@gmail.com>
2023-12-04 21:42:25 +0100

    upd task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,50 +4,29 @@ const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let timerId = setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve({ position, delay });
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject({ position, delay });
       }
     }, delay);
   });
-  promise
-    .then(value => {
-      Notify.success(value);
-      return { position: position, delay: delay };
-    })
-    .catch(error => {
-      Notify.failure(error);
-      return { position: position, delay: delay };
-    });
-
-  // return new Promise((resolve, reject) => {
-  //   let timerId = setTimeout(() => {
-  //     if (shouldResolve) {
-  //       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  //     } else {
-  //       reject(`❌ Rejected promise ${position} in ${delay}ms`);
-  //     }
-  //   }, delay);
-  // });
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  // createButton.disabled = true;
   let firstDelay = Number(form['delay'].value);
   const step = Number(form['step'].value);
   for (let i = 1; i <= form['amount'].value; i += 1) {
-    createPromise(i, firstDelay);
-    // .then(({ position, delay }) => {
-    //   Notify.success(value);
-    // })
-    // .catch(({ position, delay }) => {
-    //   Notify.failure(error);
-    // });
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
     firstDelay += step;
   }
-  // createButton.disabled = false;
 });
